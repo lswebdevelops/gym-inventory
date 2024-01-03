199d9f97612452689f793731ad0ed8edbe6ab93a
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-01-03 09:38:57 -0300

    creating router

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('hiu');
+require('dotenv').config();
+
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+
+const app = express();
+
+const PORT = 3000 || process.env.PORT;
+
+// public folder setting
+app.use(express.static('public'))
+
+// templating engine
+app.use(expressLayout)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs');
+
+// routes: 
+// instead> 
+// app.get('/', (req, res) => {
+//     res.send('hello')
+// })
+app.use('/', require('./server/routes/main'))
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on port: ${PORT}`);
+})
+
