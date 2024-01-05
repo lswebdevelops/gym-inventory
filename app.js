d0b3ab6e0c1f7358e2cdfdac81aefb231792849b
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-01-05 11:01:21 -0300

    adjusting for deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const MongoStore = require("connect-mongo");
 const app = express();
 const session = require("express-session");
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const methodOverride = require('method-override')
 
 // connect to DB
